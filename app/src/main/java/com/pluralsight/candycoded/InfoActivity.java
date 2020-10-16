e595e42ff18d5582befa40c5489727d32601a90c
christopher <techkchris4@gmail.com>
2020-10-16 15:08:48 +0200

    keep up

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,11 +1,16 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -23,9 +28,24 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public  void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri).setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+
+            this.startActivity(mapIntent);
+        } else {
+
+            Log.d("TAG","No intent to handle action");
+
+        }
+
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
